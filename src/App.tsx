6051ef9895e8e7e960a748282fefebc0134b640c
Guilherme Campos <guicamppos12@gmail.com>
2026-03-12 22:45:55 -0300

    feat: iniciando o projeto

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <>
+      <p className="text-[#000]-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. At consectetur cupiditate magnam quibusdam reiciendis provident animi debitis, nihil est harum dolore possimus facere, saepe autem ipsum expedita dicta molestias quam?</p>
+    </>
+  )
+}
+
+export default App
